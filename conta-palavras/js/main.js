45e2a4562abfb220120e350cc9cb47479cb1421a
Chili42 <rafael.economia@yahoo.com.br>
2020-03-24 00:01:48 -0300

    atualizando

--- conta-palavras/js/main.js
+++ conta-palavras/js/main.js
@@ -1,4 +1,15 @@
-var frase = $(".frase").text()
+var frase = $(".frase").text();
 var numpalavras = frase.split(" ").length;
-var tamanhoFrase = $('#tamanhoFrase')
-tamanhoFrase.text(numpalavras)
+var tamanhoFrase = $('#tamanhoFrase');
+tamanhoFrase.text(numpalavras);
+
+var campo = $(".campo-digitacao")
+campo.on("input", function(){
+    var conteudo = (campo.val())
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $('#contaPalavras').text(qtdPalavras)
+
+    var qtdCaracteres = conteudo.length
+    $("#contaCaracteres").text(qtdCaracteres)
+})
