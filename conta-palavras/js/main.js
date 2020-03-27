b39f71d8f62e21dccff8fc721fc903209e534ad6
Chili42 <rafael.economia@yahoo.com.br>
2020-03-27 14:57:19 -0300

    atualizando

--- conta-palavras/js/main.js
+++ conta-palavras/js/main.js
@@ -1,7 +1,8 @@
-var frase = $(".frase").text();
+var tempoInicial = $("#tempo").text()
+var frase = $(".frase").text()
 var numpalavras = frase.split(" ").length;
-var tamanhoFrase = $('#tamanhoFrase');
-tamanhoFrase.text(numpalavras);
+var tamanhoFrase = $('#tamanhoFrase')
+tamanhoFrase.text(numpalavras)
 
 var campo = $(".campo-digitacao")
 campo.on("input", function(){
@@ -12,4 +13,26 @@ campo.on("input", function(){
 
     var qtdCaracteres = conteudo.length
     $("#contaCaracteres").text(qtdCaracteres)
+})
+
+var tempo = $("#tempo").text()
+campo.one("focus", function(){
+    var cronometro = setInterval(function(){
+        tempo--
+        $("#tempo").text(tempo)
+        if(tempo < 1){
+        campo.attr("disabled", true)
+        clearInterval(cronometro)
+        campo.css("background-color","FireBrick")
+        }
+    },1000)
+})
+
+$('#reiniciar').click(function(){
+    campo.attr("disabled", false)
+    campo.val("")
+    $("#contaPalavras").text("0")
+    $("#contaCaracteres").text("0")
+    $("#tempo").text(tempoInicial)
+
 })
